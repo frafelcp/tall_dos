042db17c0c770ebf3b9e6c19b9d2ecce2bf90175
frafelcp <felixcastro1993@gmail.com>
2020-02-03 13:02:37 -0500

    tercero

--- tres58.java
+++ tres58.java
@@ -1,20 +1,41 @@
-import javax.swing.*;
-public class EJERCICIO_58 {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfe;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author estudiante
+ */
+public class tercero {
+
     public static void main(String[] args) {
-        int a,b,i,n;
-        a=Integer.parseInt(JOptionPane.showInputDialog("Ingrese dos números enteros positivos\nIngrese el primer número"));
-        b=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número"));
-        String v="";
-        if(a<=b){
-            for( i=a; i<=b ; i++){
-                n=(int) Math.sqrt(i);
-                if(n*n==i){
-                    v=v+i+"  ";
+        int numA, numB, raiz, v;       
+        
+        Scanner leer = new Scanner(System.in);
+        
+        v = 0;
+        
+        System.out.print("Ingrese el numero A:");
+        numA = leer.nextInt();
+        System.out.print("Ingrese el numero B:");
+        numB = leer.nextInt();
+        
+        if (numA <= numB) {
+            for (int i = numA; i <= numB; i++) {
+                raiz = (int) Math.sqrt(i);
+                if (raiz * raiz == i) {
+                    v = i;
                 }
             }
-            JOptionPane.showMessageDialog(null, "Los numeros cuadrados perfectos entre "+a+" y "+b+" son: "+v);
-        }else{
-            JOptionPane.showMessageDialog(null, "Error\nEl primer numero debe ser mayor que el segundo");
+            System.out.println("Los numeros cuadrados perfectos entre " + numA + " y " + numB + " son: " + v);
+        } else {
+            System.err.println("Error\nEl primer numero debe ser mayor que el segundo");
         }
     }
 }
+
